3c9a58d9c75c031cea2838bd085845aeb27a4cbe
Paul Novikov <paulfcdd@gmail.com>
2017-06-04 12:50:24 +0200

    style index page, add new files (fonts, node files etc), changes in custom.less

--- src/AppBundle/Controller/Front/FrontController.php
+++ src/AppBundle/Controller/Front/FrontController.php
@@ -4,23 +4,56 @@ namespace AppBundle\Controller\Front;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Config\Definition\Exception\Exception;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation as Http;
 
 class FrontController extends Controller
 {
 
+
+    const PAGES = [
+        'index' => 'Главная',
+        'zaly' => 'Залы',
+        'artisty' => 'Артисты',
+        'istoriya' => 'История',
+        'otzyvy' => 'Отзывы',
+        'kontakty' => 'Контакты',
+    ];
+
+
     /**
      * @param string $page
+     * @param Http\Request $request
      * @return Response
-     * @Route("/{page}", name="front.renderMenu")
+     * @Route("/{page}",
+     *     name="front.renderMenu"
+     * )
      */
-    public function mainMenuPageRenderAction(string $page = null) {
+    public function mainMenuPageRenderAction(string $page = null, Http\Request $request) {
+
+        $response = new Http\Response();
+
+        $env = $this->container->getParameter('kernel.environment');
 
         if(!$page) {
             $page = 'index';
         }
 
-        return $this->render(':default/front/page:'.$page.'.html.twig');
+        if (!in_array($page, array_flip(self::PAGES))) {
+
+            $errorMessage = '';
+
+            if ($env == 'dev') {
+                $errorMessage = 'Шаблон '.$page.'.html.twig не найден в папке app/Resources/views/default/front/page';
+            }
+
+            throw new Exception($errorMessage, 404);
+        }
+
+        return $this->render(':default/front/page:'.$page.'.html.twig', [
+            'page' => $page,
+        ]);
     }
 }

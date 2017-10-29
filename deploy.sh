2673e1fb0e76e3fc867627e6c590244076f5b241
Paul <paulfcdd@gmail.com>
2017-10-29 23:21:12 +0100

    add file for deployment

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+echo '========= Pull new changes from repository ==========='
+git pull
+
+echo '========= Updating database structure ==========='
+bin/console doctrine:schema:update --force
+
+echo '========= Clear cache ==========='
+bin/console cache:clear --no-warmup
+bin/console cache:clear --env=prod
+
+echo '========= Deploy finushed! ==========='
